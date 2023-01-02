ba024447626c2dfde833b03d16b0dcf11c2bbeaa
h.eiri <hanif.js93@gmail.com>
2023-01-02 09:19:04 +0330

    add material and test it working

--- projects/multi/src/lib/multi.module.ts
+++ projects/multi/src/lib/multi.module.ts
@@ -3,13 +3,18 @@ import { MultiComponent } from './multi.component';
 import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [MultiComponent],
   imports: [
     CommonModule,
     HttpClientModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatButtonModule
   ],
   exports: [MultiComponent]
 })
